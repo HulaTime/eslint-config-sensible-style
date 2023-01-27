19cadeb9c7612e64ef83fcd15b3bb9f6491089fc
hulatime <hulatime@tutanota.com>
2023-01-27 22:54:57 +0000

    chore: move rules back to main file

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const typescriptRules = require('./typescript-rules');
-const defaultRules = require('./default-rules');
-
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -19,9 +16,48 @@ module.exports = {
       node: true,
     }
   },
-  rules: defaultRules,
-  overrides: [{
-    files: ['*.ts', '*.tsx'],
-    rules: typescriptRules,
-  }],
+  rules: {
+    'array-bracket-spacing': ['error', 'never'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'eol-last': ['error', 'always'],
+    indent: ['error', 2, {SwitchCase: 1}],
+    'import/order': ['error', {
+      'newlines-between': 'always',
+    }],
+    'lines-between-class-members': 'error',
+    'max-len': ['error', {
+      code: 110,
+      ignoreStrings: true,
+    }],
+    'newline-per-chained-call': ['error', {
+      ignoreChainWithDepth: 2,
+    }],
+    'no-irregular-whitespace': ['error'],
+    'no-whitespace-before-property': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'object-curly-newline': ['error', {
+      minProperties: 4,
+      multiline: true,
+    }],
+    'object-property-newline': ['error', {
+      allowAllPropertiesOnSameLine: true,
+    }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-multi-spaces': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-extra-semi': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi'
+        },
+        singleline: {
+          delimiter: 'semi'
+        },
+      }
+    ],
+  },
 };
