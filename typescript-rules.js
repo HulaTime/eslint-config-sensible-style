be95f41821998bb6a26d980636a5c588af2879ee
hulatime <hulatime@tutanota.com>
2023-01-27 22:11:17 +0000

    chore: disable typescript no unnecessary semi colon

--- typescript-rules.js
+++ typescript-rules.js
@@ -1,5 +1,6 @@
 module.exports = {
   '@typescript-eslint/explicit-function-return-type': 'error',
+  '@typescript-eslint/no-extra-semi': 'off',
   '@typescript-eslint/member-delimiter-style': ['error', {
     multiline: {
       delimiter: 'semi'
